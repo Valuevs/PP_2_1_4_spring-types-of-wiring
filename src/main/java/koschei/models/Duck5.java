0c153c14e9d5806365d59d17575f682b0a13934f
Valuev Semyon <valuevs466@gmail.com>
2024-06-24 01:28:57 +0300

    черновой вариант

--- src/main/java/koschei/models/Duck5.java
+++ src/main/java/koschei/models/Duck5.java
@@ -1,12 +1,23 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Duck5 {
 
+    @Qualifier("egg6")
+    @Autowired
+    private Egg6 egg;
+
     @Override
     public String toString() {
-        return ", в утке яйцо " + "";
+        return ", в утке яйцо " + egg.toString();
+    }
+
+    public void setEgg(Egg6 egg) {
+        this.egg = egg;
     }
 }
+
